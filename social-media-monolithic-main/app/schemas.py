72c8d98cd3ad579bc57e0f8de017fa610954a5e7
poonampoonia01 <154328372+poonampoonia01@users.noreply.github.com>
2026-09-02 11:56:13 +0530

    Update schemas.py

--- social-media-monolithic-main/app/schemas.py
+++ social-media-monolithic-main/app/schemas.py
@@ -6,7 +6,7 @@ from pydantic.types import conint
 class PostBase(BaseModel):
     title: str
     content: str
-    published: bool = True 
+    published: bool = True
 
 class PostCreate(PostBase):
     pass
